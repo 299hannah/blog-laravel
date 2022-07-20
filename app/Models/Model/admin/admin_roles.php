8e3519f531a6682f532485d7113fa87f098d289c
299hannah <hannahmwangiwambui@gmail.com>
2022-07-20 17:48:46 +0300

    db creation

---
+++ app/Models/Model/admin/admin_roles.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Model\admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class admin_roles extends Model
+{
+    use HasFactory;
+}
