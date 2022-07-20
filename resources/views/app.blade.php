a499efd7619fff2caab5f013c3965ba21e9c3b95
299hannah <hannahmwangiwambui@gmail.com>
2022-07-20 16:31:23 +0300

    Creating a blog skeleton project while using template, also configuring the routes and making some functionalities dynamic.

---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+   @include('layout/head')
+    </head>
+    <body>
+        <!-- Navigation-->
+        @include('layout/header') 
+       
+
+        <!-- Main Content-->
+        @section('main-content')
+            @show
+
+       <!-- Footer-->
+        @include('layout/footer')
+
+</body>
+</html>
