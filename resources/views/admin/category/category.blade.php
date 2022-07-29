13bdd29c50c089f72c586e7ce166cc4c16ddcc24
299hannah <hannahmwangiwambui@gmail.com>
2022-07-29 10:33:56 +0300

    creting route lists using resources with controllers

--- resources/views/admin/category/category.blade.php
+++ resources/views/admin/category/category.blade.php
@@ -2,4 +2,51 @@
 
 @section('main-content')
 
+
+<div class="content-wrapper">
+
+    <div class="card ">
+      <div class="row justify-content-center">
+  
+        <!-- /.card-header -->
+        <!-- form start -->
+  
+  
+        {{-- <form> --}}
+          <div class="col-lg-6  " style="align-items: center">
+  
+            <div class="card-header">
+              <h3 class="card-title">Titles</h3>
+            </div>
+            <div class="card-body ">
+  
+              <div class="form-group">
+                <label for="name">Category title</label>
+                <input type="text" class="form-control" name="name" id="name" placeholder="Write your Category Title">
+              </div>
+        
+              <div class="form-group">
+                <label for="slug">Category Slug</label>
+                <input type="text" class="form-control" name="slug" id="slug" placeholder="Write your slug">
+              </div>
+  
+            </div>
+            <div class=" card-footer text-center form-group col-lg-12">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+          </div>
+
+  
+        </div>
+
+  
+     
+  
+          {{--
+        </form> --}}
+  
+
+
+
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,41 @@
 <?php
 
+use GuzzleHttp\Promise\Create;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('blog');
-});
+Route::get('/','App\Http\Controllers\HomeController@index');
+// Route::get('/',[PostController::class,'index']);
+Route::get('/','App\Http\Controllers\PostController@index');
 
-Route::get('post',function(){
-    return view('post');
-})->name('post');
+// Route::group(['namespace'=> 'App\Http\Controllers\Admin\
+// '],function(){
 
+//     Route::resource('admin/post','PostController');
+//     // Route::resource('admin/post','TagController');
+//     // Route::resource('admin/post','CategoryController');
+    
+// });
 
-Route::get('admin/home',function(){
-    return view('admin.home');
-})->name('post');
+// use App\Http\Controllers\Admin\CategoryController;
+ 
+// Route::resource('admin/post', CategoryController::class);
+// Route::resource('admin/post','App\Http\Controllers\PostController');
+// Route::resource('admin/post','App\Http\Controllers\TagController');
+Route::resource('admin/post','App\Http\Controllers\Admin\CategoryController');
 
-Route::get('admin/post',function(){
-    return view('admin/post/post');
-});
+// Route::get('admin/home',function(){
+//     return view('admin.home');
+// })->name('post');
 
-Route::get('admin/tag',function(){
-    return view('admin.tag.tag');
-});
+// Route::get('admin/post',function(){
+//     return view('admin/post/post');
+// });
 
-Route::get('admin/category',function(){
-    return view('admin.category.category');
-});
+// Route::get('admin/tag',function(){
+//     return view('admin.tag.tag');
+// });
+
+// Route::get('admin/category',function(){
+//     return view('admin.category.category');
+// });
